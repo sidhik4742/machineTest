b92c318523403c9e77db6fb42185e5b8204b18a4
Sidhik AR <sidhik64742@gmail.com>
2021-05-21 00:42:40 +0530

    data base changed to atlas

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-const MongoClient = require("mongodb").MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 // const objectId = require("mongodb").ObjectID;
 // const config = require("../config/config");
 
@@ -9,19 +9,20 @@ let state = {
 module.exports = {
   dbConnect: (callback) => {
     try {
-      const url = "mongodb://localhost:27017";
-      const dbName = "productDB";
-      MongoClient.connect(url, { useUnifiedTopology: true }, (error, db) => {
+      const url =
+        'mongodb+srv://sidhik:letmein64742@cluster0.wgcac.mongodb.net/productDB?retryWrites=true&w=majority'; //"mongodb://localhost:27017";
+      const dbName = 'productDB';
+      MongoClient.connect(url, {useUnifiedTopology: true}, (error, db) => {
         if (error) {
           return callback(error);
         }
-        console.log("Connected successfully to server");
+        console.log('Connected successfully to server');
         //   console.log(db);
         state.db = db.db(dbName);
         callback();
       });
     } catch (error) {
-      console.log("Database connection error" + error);
+      console.log('Database connection error' + error);
     }
   },
 };
